c379454165c7c74d51132806ca6c758070c67b2a
Adwitiya Mourya <adwitiya.code@gmail.com>
2023-11-28 12:03:57 +0530

    Module.exports, import message - comments added

--- compile.js
+++ compile.js
@@ -1,7 +1,11 @@
+// Below are modules that allows us to find the exact location of our files in this directory
+// Path is to fetch path
+// Fs is a filesystem module of node.js that allows us to to interact with the filesystem.
 const path = require("path");
 const fs = require("fs");
 const solc = require("solc");
 
+//* __dirname is kind of a constant defined in JS, that fetches the current path of directory we're in.
 const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
 const source = fs.readFileSync(inboxPath, "utf8");
 
@@ -22,6 +26,11 @@ const input = {
 };
 
 console.log(JSON.parse(solc.compile(JSON.stringify(input))).contracts);
+/*
+ * Module.exports is a special variable and a property
+ * so that we can use the compiled output anywhere in our application
+ * In our case, the compiled output is the ABI and the interface.
+ */
 module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
   "Inbox.sol"
 ].Inbox;
